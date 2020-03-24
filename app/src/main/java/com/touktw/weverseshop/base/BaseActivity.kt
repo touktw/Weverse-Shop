6caa181e769273de64dc41c212c473450962b0bc
tae.kim <tae.kim@neofect.com>
2020-03-24 12:02:00 +0900

    add base activity

---
+++ app/src/main/java/com/touktw/weverseshop/base/BaseActivity.kt
@@ -0,0 +1,10 @@
+package com.touktw.weverseshop.base
+
+import androidx.appcompat.app.AppCompatActivity
+
+/**
+ * Created by taekim on 2020-03-24
+ */
+
+
+abstract class BaseActivity : AppCompatActivity()
